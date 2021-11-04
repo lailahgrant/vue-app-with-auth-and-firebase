e218aee32957127687ee45ca0d1f618b1e394bf1
Lailah Grant <lailahgrant19@gmail.com>
2021-11-04 14:20:11 +0300

    added firebase.js file

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDISQM9KWzAccqvwc_aAXU8TuRneMw1T5E",
+    authDomain: "vue-firebase-auth-d2658.firebaseapp.com",
+    projectId: "vue-firebase-auth-d2658",
+    storageBucket: "vue-firebase-auth-d2658.appspot.com",
+    messagingSenderId: "190312009382",
+    appId: "1:190312009382:web:72b422a72d55fd7a0d6e67",
+    measurementId: "G-6GY78VELRH"
+};
+
+// Initialize Firebase 
+initializeApp(firebaseConfig);
